a0f538559df7d735b843938895911fa0f111a963
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2018-08-04 21:18:48 +0200

    Use window.process.env

--- packages/@dotsync/core/lib/configdir.js
+++ packages/@dotsync/core/lib/configdir.js
@@ -4,14 +4,14 @@ const path = require('path');
 module.exports = (appname) => {
   let dir;
   const platform = process.platform;
-  const home = process.env.HOME; // TODO: Dotenv
+  const home = window.process.env.HOME;
 
   if (platform == 'win32') {
-    dir = process.env.APPDATA;
+    dir = window.process.env.APPDATA;
   } else if (platform == 'darwin') {
     dir = path.join(home, 'Library', 'Application Support');
   } else {
-    dir = process.env.XDG_CONFIG_HOME || path.join(home, '.config');
+    dir = window.process.env.XDG_CONFIG_HOME || path.join(home, '.config');
   }
 
   const configdir = path.join(dir, appname);
